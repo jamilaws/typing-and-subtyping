e87e77e0cf067edc005e707556f8edc76a7509bf
DELL-XPS-17\dendredi <david.endredi@beskgroup.com>
2022-01-26 20:46:51 +0100

    Adjust material in notes

--- src/app/model/typing/types/abstract-type.ts
+++ src/app/model/typing/types/abstract-type.ts
@@ -1,6 +1,14 @@
 export abstract class AbstractType {
     abstract toString(): string;
 
+    /**
+     * TODO: Implement subtyping!
+     * 
+     * e.g.: Compare NAMES when comparing structs
+     * 
+     * @param other 
+     * @returns 
+     */
     public equals(other: AbstractType): boolean{          
         return this.toString() === other.toString();
     }
